fd65dfc3e4f4d4f9b2221a9ba4c757259b333481
Souayah-Malek12 <malek.souayah@isimg.tn>
2025-10-24 20:40:48 +0100

    adding filter on transactions Page

--- src/app/pages/transactions-list/transactions-list.ts
+++ src/app/pages/transactions-list/transactions-list.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { TransactionResponse, TransactionService } from './../../services/transService/transaction-service';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-transactions-list',
   standalone : true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './transactions-list.html',
   styleUrl: './transactions-list.scss'
 })
@@ -14,7 +16,9 @@ export class TransactionsList implements OnInit{
   transactionsList : TransactionResponse[]  = [] ;
   loading = true;
   error: string | null = null;
-
+  filterType: string = '';
+  minAmount : number = 0;
+  accountId : number= 0;
 
   constructor(private transactionService :TransactionService ){}
 
@@ -37,6 +41,28 @@ export class TransactionsList implements OnInit{
     })
 
   }
+
+
+  getFilteredTransaction(): TransactionResponse[] {
+
+    let filteredList = this.transactionsList;
+
+
+    if(this.accountId  !==0  && this.accountId>0) {
+      filteredList =  this.transactionsList.filter( t => (parseFloat(this.accountId.toString()) === t.accountId ) )
+    }
+    if(this.filterType !==''){
+      filteredList =  this.transactionsList.filter(t => (t.type) === this.filterType)
+    }
+
+    if(this.minAmount >0){
+      filteredList = filteredList.filter( t => t.amount > this.minAmount) ;
+    }
+
+
+    return filteredList;
+  }
+
 
 
 }
